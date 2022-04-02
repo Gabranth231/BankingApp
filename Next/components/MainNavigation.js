e10488c148a1076b6c69a02aa2728c40018282d1
josbrink <G00362643@gmit.ie>
2022-04-02 16:25:37 +0100

    Navbar

--- Next/components/MainNavigation.js
+++ Next/components/MainNavigation.js
@@ -1,28 +1,27 @@
 import Link from "next/link";
-import classes from "./MainNavigation.module.css";
-import { Card, Avatar, Button } from '@nextui-org/react';
-import { useContext } from 'react';
-import DataContext from '../store/data-store.js'
+import { Button, Grid } from "@nextui-org/react";
 
 function MainNavigation(props) {
-  // const dataCtx = useContext(DataContext)
-  // let noOfEmployees = dataCtx.getNoEmployees()
-  
   return (
     <>
-      <Card bordered shadow={false} hoverable css={{ mw: "30%" }}>
-        <div className={classes.mainDiv}>
-          <div className={classes.linkDiv}>
-            <Link href="/userpage"><a><Button shadow color="gradient" auto>UserPage</Button></a></Link>
-          </div>
-          <div className={classes.linkDiv}>
-            <Link href="/transactions"><a><Button shadow color="gradient" auto>Transactions</Button></a></Link>
-          </div>
-          <div className={classes.linkDiv}>
-            <Link href="/"><a><Button shadow color="gradient" auto>Logout</Button></a></Link>
-          </div>
-        </div>
-      </Card>
+      <Grid.Container direction="vertical">
+        <Grid md={12}>
+          <Button.Group size="xl">
+            <Link href="/userpage">
+              <Button>User Page</Button>
+            </Link>
+            <Link href="/transactions">
+              <Button>Transactions</Button>
+            </Link>
+            <Link href="/TransferPage">
+              <Button>Transfer</Button>
+            </Link>
+            <Link href="/">
+              <Button>Logout</Button>
+            </Link>
+          </Button.Group>
+        </Grid>
+      </Grid.Container>
     </>
   );
 }
