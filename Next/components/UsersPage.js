f6a90c6af1a1206774507e30d22592e5611cfde8
Gabranth231 <boycie3164@gmail.com>
2022-04-03 17:01:41 +0100

    First database access code

--- Next/components/UsersPage.js
+++ Next/components/UsersPage.js
@@ -1,10 +1,95 @@
 import React from 'react'
+import {Container, Row, Col, Card, Text, Spacer, Table} from "@nextui-org/react"
 function UsersPage() 
 {
+  const columns = [
+    {
+      key: "name",
+      label: "NAME",
+    },
+    {
+      key: "accountid",
+      label: "AccountID",
+    },
+  ];
+  const rows = [
+    {
+      key: "1",
+      name: "Tony Reichert",
+      accountid: "123",
+    },
+    {
+      key: "2",
+      name: "Zoey Lang",
+      accountid: "456",
+    },
+    {
+      key: "3",
+      name: "Jane Fisher",
+      accountid: "789",
+    },
+    {
+      key: "4",
+      name: "William Howard",
+      accountid: "101112",
+    },
+  ];
   return (
-    <section>
-      <h1>Users</h1>
-    </section>
+    
+    <Container gap={0}>
+      <Spacer y={2}/>
+      <Row gap={1}>
+        <Col>
+          <Container gap={0}>
+          <Row gap={1}>
+            <Card color="success">
+              <Text h6 size={15} color="white" css={{ m: 0 }}>
+                Account Details
+              </Text>
+            </Card>
+            </Row>
+            <Spacer y={1}/>
+            <Row gap={1}>
+            <Card color="secondary">
+              <Text h6 size={15} color="white" css={{ m: 0 }}>
+                Name: {}
+              </Text>
+            </Card>
+            </Row>
+            <Spacer y={1}/>
+            <Row gap={1}>
+            <Card color="secondary">
+              <Text h6 size={15} color="white" css={{ m: 0 }}>
+                Balence: {}
+              </Text>
+            </Card>
+            </Row>
+          </Container>
+        </Col>
+        <Col>
+          <Table
+            aria-label="Example table with dynamic content"
+            css={{
+              height: "auto",
+              minWidth: "100%",
+            }}
+          >
+            <Table.Header columns={columns}>
+              {(column) => (
+                <Table.Column key={column.key}>{column.label}</Table.Column>
+              )}
+            </Table.Header>
+            <Table.Body items={rows}>
+              {(item) => (
+                <Table.Row key={item.key}>
+                  {(columnKey) => <Table.Cell>{item[columnKey]}</Table.Cell>}
+                </Table.Row>
+              )}
+            </Table.Body>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
   );
 }
   
--- Node/routes/index.js
+++ Node/routes/index.js
@@ -8,10 +8,19 @@ let blogSchema = new Schema(
   {
     noEmployees: String
   },
-  { collection: "employees" }
+  { collection: "employees" } 
 );
 let EmployeeModel = mongoose.model("employeeInfo", blogSchema);
 
+let userSchema = new Schema(
+  {
+    userName: String,
+    balence: String,
+    userContactsIDs: Array,
+    transactionsIDs: Array,
+  }
+)
+let UserModel = mongoose.model("userInfo",userSchema);
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" })
@@ -23,8 +32,22 @@ router.post("/setNoEmployees", function (req, res, next) {
   data.save()
   res.end()
 });
+router.post("/addUser", function (req, res, next) {
+  console.log(req.body)
+  var data = new UserModel(req.body)
+  data.save()
+  res.end()
+});
 
 router.post("/getNoEmployees", function (req, res, next) {
+  EmployeeModel.find().then(function (docs) {
+    let theDoc = docs[docs.length-1]
+     console.log("/getNoEmployees: " + theDoc)
+     // theDoc = JSON.stringify(theDoc)
+    res.status(200).json(theDoc)
+  });
+});
+router.post("/getUser", function (req, res, next) {
   EmployeeModel.find().then(function (docs) {
     let theDoc = docs[docs.length-1]
      console.log("/getNoEmployees: " + theDoc)
