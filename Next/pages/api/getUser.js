7e6ace0e948ac7de8808d13262415c759374416e
Gabranth231 <boycie3164@gmail.com>
2022-04-03 18:09:16 +0100

    Removing unused files

--- Next/pages/api/getUser.js
+++ Next/pages/api/getUser.js
@@ -1,7 +1,7 @@
 // https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
-    await fetch('http://localhost:8000/getNoEmployees', {
+    await fetch('http://localhost:8000/getUser', {
         method: 'POST'
     })
     .then((res) => res.json())
